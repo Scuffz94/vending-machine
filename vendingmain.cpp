4dbdf265b5653b5bcbb4ba69e46c80a705a4e76c
Christopher Wells <Windssoulx7@gmail.com>
2016-11-27 19:06:38 -0800

    Setup Commit

---
+++ vendingmain.cpp
@@ -0,0 +1,14 @@
+/*  Program:     vendingmain.cpp
+    Author:      Christopher Wells, Kimberly Arambulo, <Name>
+    Class:	     CSCI 140
+    Date:	     November 20. 2016
+    Description: Primary driver for vending machine program.
+
+    I certify that the code below is my own work.
+
+	Exception(s): N/A
+*/
+
+/*TODO: Create primary classes "Drink" & "Snack".
+For credit cards, make class called Credit that uses Luhns algorithm check.
+*/
